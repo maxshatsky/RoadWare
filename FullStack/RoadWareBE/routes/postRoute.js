5bd6ea1848bfbdf79eda69d01100667a39e06b04
shlomozion <jordan23zion@gmail.com>
2023-05-31 21:17:05 +0300

    added route to data science database

--- FullStack/RoadWareBE/routes/postRoute.js
+++ FullStack/RoadWareBE/routes/postRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 const dangerController = require("../controllers/postController");
 
 router.post("/level", dangerController.getDangerLevels);
