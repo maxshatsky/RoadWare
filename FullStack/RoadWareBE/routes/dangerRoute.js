1264198a542c38aa2b15d78b6c9616d2535806ef
Noa Sela <noalina.sela@gmail.com>
2023-06-01 10:53:06 +0300

    WIP

--- FullStack/RoadWareBE/routes/dangerRoute.js
+++ FullStack/RoadWareBE/routes/dangerRoute.js
@@ -1,17 +1,6 @@
-<<<<<<< HEAD:FullStack/RoadWareBE/routes/dangerRoute.js
 const express = require("express");
 const router = express.Router();
 const dangerController = require("../controllers/dangerController");
 router.post("/level", dangerController.getDangerLevels);
 
 module.exports = router;
-=======
-const express = require("express");
-const router = express.Router();
-
-const dangerController = require("../controllers/postController");
-
-router.post("/level", dangerController.getDangerLevels);
-
-module.exports = router;
->>>>>>> origin/server-update:FullStack/RoadWareBE/routes/postRoute.js
